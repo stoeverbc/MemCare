3e67847e4827bd8733b7ac3c6a398c564460e9c5
stoeverbc <stoever@bc.edu>
2025-06-26 19:53:27 -0600

    Initial Commit

---
+++ MemCare/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MemCare
+//
+//  Created by Christian Kurt Stoever on 6/26/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MemCare/MemCareApp.swift
@@ -0,0 +1,32 @@
+//
+//  MemCareApp.swift
+//  MemCare
+//
+//  Created by Christian Kurt Stoever on 6/26/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MemCareApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
